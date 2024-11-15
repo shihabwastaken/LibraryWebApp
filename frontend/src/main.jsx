d61836eb3086631b80fbdd9d50b9216bcfe3d88a
ArafAhmed1 <arafahmed541@gmail.com>
2024-11-15 23:51:38 +0600

    fix navbar background bug and bookshelf css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './styles/App.css'
 import './styles/Bookshelf.css'
 import './styles/Header.css'
 import './styles/Footer.css'
