5977ec949295696494e0f3a28812b733a78c922e
ArafAhmed1 <arafahmed541@gmail.com>
2024-12-29 07:48:24 +0600

    changing url

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000';  // Your backend's base URL
+export const BASE_URL = 'https://librarywebapp-nlbi.onrender.com';  // Your backend's base URL
 export const USERS_URL = '/api/users';
